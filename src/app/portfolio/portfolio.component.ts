5ce6de1335b9088b524a1fc36d5e56462ededd52
Aliya Miranda <miranda.aliya@gmail.com>
2021-07-26 22:30:24 +0800

    added simple carousel

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,6 +8,10 @@ import { Portfolio } from '../_model/portfolio';
 })
 export class PortfolioComponent implements OnInit {
 	portfolio: Portfolio[] = [];
+	maxView: number = 3;
+	pages: number;
+	currentPage: number = 0;
+	portfolioView: Portfolio[] = [];
 
 	constructor() { 
 		this.portfolio.push({
@@ -24,9 +28,56 @@ export class PortfolioComponent implements OnInit {
 			preview_image: "../../assets/placeholder.png",
 			url: null
 		})
+		this.portfolio.push({
+			title: "Test Title 3",
+			short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla.",
+			details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla.",
+			preview_image: "../../assets/placeholder.png",
+			url: null
+		})
+		this.portfolio.push({
+			title: "Test Title 4",
+			short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla.",
+			details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla.",
+			preview_image: "../../assets/placeholder.png",
+			url: null
+		})
 	}
 
 	ngOnInit(): void {
+		this.pages = Math.floor(this.portfolio.length / this.maxView)
+		this.updatePortfolioView()
+	}
+
+	nextPage() {
+		if (this.currentPage < this.pages) {
+			this.currentPage = this.currentPage + 1
+		} else {
+			this.currentPage = 0
+		}
+		this.updatePortfolioView()
+	}
+
+	updatePortfolioView(){
+		let articleIndex = this.currentPage * this.maxView
+		if (articleIndex + this.maxView > this.portfolio.length) {
+			this.portfolioView = this.portfolio.slice(articleIndex, this.portfolio.length)
+			console.log(articleIndex, this.portfolio.length)
+			console.log(this.portfolioView)
+		} else {
+			this.portfolioView = this.portfolio.slice(articleIndex, articleIndex + this.maxView)
+			console.log(articleIndex, articleIndex + this.maxView)
+			console.log(this.portfolioView)
+		}
+	}
+
+	previousPage() {
+		if (this.currentPage == 0) {
+			this.currentPage = this.pages
+		} else {
+			this.currentPage = this.currentPage - 1
+		}
+		this.updatePortfolioView()
 	}
 
 }
