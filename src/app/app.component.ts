04b8c0f8c35f32ac411de880d892dc6c8849db1a
Aliya Miranda <miranda.aliya@gmail.com>
2021-08-26 17:04:04 +0800

    added google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import{ Router, NavigationEnd } from '@angular/router';
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'madreloidpx';
+  constructor(public router: Router){   
+    this.router.events.subscribe(event => {
+       if(event instanceof NavigationEnd){
+           gtag('config', 'G-Y4NP363TE8', 
+                 {
+                   'page_path': event.urlAfterRedirects
+                 }
+                );
+        }
+     }
+  )}
 }
