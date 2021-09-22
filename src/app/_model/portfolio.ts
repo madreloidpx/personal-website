e8cced1d125ad02ac50d44034edd88546be63bf8
Aliya Miranda <miranda.aliya@gmail.com>
2021-09-22 22:05:02 +0800

    moved to react. simple initialized app

--- src/app/_model/portfolio.ts
+++
@@ -1,8 +0,0 @@
-export interface Portfolio {
-    title: string;
-    details: string;
-    short_description: string;
-    preview_image: string;
-    collection: string[] | null;
-    url: string | null;
-}
