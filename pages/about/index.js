22da6f25003916c74dc965f117e550194a16ddd0
Aliya Miranda <miranda.aliya@gmail.com>
2021-12-09 18:22:07 +0800

    Added about details

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,13 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import styles from '../../styles/About.module.css'
+
+const imgLoader = ({src, width, quality}) => { 
+  const router = useRouter();
+  return `${router.basePath}/${src}?w=${width}&q=${quality || 75}` 
+}
 
 export default function About(req, res) {
   return (
@@ -10,47 +18,70 @@ export default function About(req, res) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.logo}>
-          <div className={styles.icon}>
-            <svg className={styles.icon} width="50" height="50" viewBox="0 0 269 269" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="134.5" cy="134.5" r="134" fill="#867CBF" stroke="#2D2940"/>
-              <line x1="228.75" y1="38.4196" x2="38.4194" y2="228.75" stroke="#2D2940"/>
-              <line x1="40.9573" y1="40.2502" x2="228.75" y2="228.043" stroke="#2D2940"/>
-            </svg>
-          </div>
-          <h1>MADRELOIDPX</h1>
-        </div>
-        <div className={styles.nav}>
-          <ul className={styles.nav_list}>
-            <li className={styles.upper}>
-              <div className={styles.flower}>
-                <svg width="50" height="50" viewBox="0 0 94 93" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M47.0403 46.5574C37.8936 36.3837 20.8479 4.46978 42.198 1.23834C66.6403 -2.46114 50.2685 38.0023 47.0403 46.5574Z" stroke="#2D2940"/>
-                  <path d="M47.0753 46.9572C56.9293 56.4436 76.2236 87.0432 55.1613 91.8086C31.0483 97.2643 44.4703 55.7232 47.0753 46.9572Z" stroke="#2D2940"/>
-                  <path d="M47.0104 46.5816C60.3471 43.618 96.441 44.4819 88.7186 64.7007C79.8777 87.8479 52.8416 53.6157 47.0104 46.5816Z" stroke="#2D2940"/>
-                  <path d="M47.1307 46.7291C44.0332 60.0699 27.6598 92.3358 12.8127 76.6159C-4.18492 58.6191 38.2848 48.9627 47.1307 46.7291Z" stroke="#2D2940"/>
-                  <path d="M47.2959 46.5512C34.0762 50.0021 -2.02507 50.4627 4.95454 29.9742C12.9451 6.51815 41.2119 39.7357 47.2959 46.5512Z" stroke="#2D2940"/>
-                  <path d="M47.1655 46.6742C50.9789 33.5212 69.0709 2.19172 83.0473 18.6948C99.048 37.5882 56.119 44.9242 47.1655 46.6742Z" stroke="#2D2940"/>
-                </svg>
+      <main id="about" className={styles.main}>
+        <div className={styles.left}>
+          <section id="logo" className={styles.logo}>
+              <div className={styles.icon}>
+                <Link href="/">
+                  <svg className={styles.icon} width="35" height="35" viewBox="0 0 269 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="134.5" cy="134.5" r="134" fill="#867CBF" stroke="#2D2940"/>
+                    <line x1="228.75" y1="38.4196" x2="38.4194" y2="228.75" stroke="#2D2940"/>
+                    <line x1="40.9573" y1="40.2502" x2="228.75" y2="228.043" stroke="#2D2940"/>
+                  </svg>
+                </Link>
               </div>
-              <h2><a href="#">About</a></h2>
-            </li>
-            <li className={styles.lower}>
-              <div className={styles.flower}>
-                <svg width="50" height="50" viewBox="0 0 94 93" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M47.0403 46.5574C37.8936 36.3837 20.8479 4.46978 42.198 1.23834C66.6403 -2.46114 50.2685 38.0023 47.0403 46.5574Z" stroke="#2D2940"/>
-                  <path d="M47.0753 46.9572C56.9293 56.4436 76.2236 87.0432 55.1613 91.8086C31.0483 97.2643 44.4703 55.7232 47.0753 46.9572Z" stroke="#2D2940"/>
-                  <path d="M47.0104 46.5816C60.3471 43.618 96.441 44.4819 88.7186 64.7007C79.8777 87.8479 52.8416 53.6157 47.0104 46.5816Z" stroke="#2D2940"/>
-                  <path d="M47.1307 46.7291C44.0332 60.0699 27.6598 92.3358 12.8127 76.6159C-4.18492 58.6191 38.2848 48.9627 47.1307 46.7291Z" stroke="#2D2940"/>
-                  <path d="M47.2959 46.5512C34.0762 50.0021 -2.02507 50.4627 4.95454 29.9742C12.9451 6.51815 41.2119 39.7357 47.2959 46.5512Z" stroke="#2D2940"/>
-                  <path d="M47.1655 46.6742C50.9789 33.5212 69.0709 2.19172 83.0473 18.6948C99.048 37.5882 56.119 44.9242 47.1655 46.6742Z" stroke="#2D2940"/>
-                </svg>
-              </div>
-              <h2><a href="#">Works</a></h2>
-            </li>
-          </ul>
+              <h1 className={styles.webname}><Link href="/"><a>MADRELOIDPX</a></Link></h1>
+          </section>
+          <section id="content" className={styles.content}>
+            <div className={styles.container}>
+              <Image loader={imgLoader} src="img/Image Placeholder.svg" width={400} height={400}/>
+            </div>
+          </section>
         </div>
+        <section id="nav" className={styles.nav}>
+          <nav className={styles.nav_list}>
+            <ul>
+              <li className={styles.upper}>
+                <div className={styles.flower}>
+                  <svg width="50" height="50" viewBox="0 0 94 93" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M47.0403 46.5574C37.8936 36.3837 20.8479 4.46978 42.198 1.23834C66.6403 -2.46114 50.2685 38.0023 47.0403 46.5574Z" stroke="#2D2940"/>
+                    <path d="M47.0753 46.9572C56.9293 56.4436 76.2236 87.0432 55.1613 91.8086C31.0483 97.2643 44.4703 55.7232 47.0753 46.9572Z" stroke="#2D2940"/>
+                    <path d="M47.0104 46.5816C60.3471 43.618 96.441 44.4819 88.7186 64.7007C79.8777 87.8479 52.8416 53.6157 47.0104 46.5816Z" stroke="#2D2940"/>
+                    <path d="M47.1307 46.7291C44.0332 60.0699 27.6598 92.3358 12.8127 76.6159C-4.18492 58.6191 38.2848 48.9627 47.1307 46.7291Z" stroke="#2D2940"/>
+                    <path d="M47.2959 46.5512C34.0762 50.0021 -2.02507 50.4627 4.95454 29.9742C12.9451 6.51815 41.2119 39.7357 47.2959 46.5512Z" stroke="#2D2940"/>
+                    <path d="M47.1655 46.6742C50.9789 33.5212 69.0709 2.19172 83.0473 18.6948C99.048 37.5882 56.119 44.9242 47.1655 46.6742Z" stroke="#2D2940"/>
+                  </svg>
+                </div>
+                <h2>About</h2>
+              </li>
+              <li className={styles.middle}>
+                <h3 className={styles.emphasize}>Hi! I'm Aliya</h3>
+                <h3 className={styles.emphasize}>Web Developer</h3>
+                <h3 className={styles.emphasize}>Philippines</h3>
+                <div className={styles.social_links}>
+                  <a target="_blank" href="https://www.behance.net/madreloidpx/"><Image loader={imgLoader} src="img/behance-1.svg" width={50} height={50}/></a>
+                  <a target="_blank" href="https://github.com/madreloidpx"><Image loader={imgLoader} src="img/github.png" width={50} height={50}/></a>
+                  <a target="_blank" href="https://www.linkedin.com/in/madreloidpx/"><Image loader={imgLoader} src="img/linkedin.png" width={50} height={50}/></a>
+                  <a target="_blank" href="https://www.reddit.com/user/madreloidpx"><Image loader={imgLoader} src="img/reddit.png" width={50} height={50}/></a>
+                  <a target="_blank" href="https://twitter.com/madreloidpx"><Image loader={imgLoader} src="img/twitter.png" width={50} height={50}/></a>
+                </div>
+              </li>
+              <li className={styles.lower}>
+                <div className={styles.flower}>
+                  <svg width="50" height="50" viewBox="0 0 94 93" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M47.0403 46.5574C37.8936 36.3837 20.8479 4.46978 42.198 1.23834C66.6403 -2.46114 50.2685 38.0023 47.0403 46.5574Z" stroke="#2D2940"/>
+                    <path d="M47.0753 46.9572C56.9293 56.4436 76.2236 87.0432 55.1613 91.8086C31.0483 97.2643 44.4703 55.7232 47.0753 46.9572Z" stroke="#2D2940"/>
+                    <path d="M47.0104 46.5816C60.3471 43.618 96.441 44.4819 88.7186 64.7007C79.8777 87.8479 52.8416 53.6157 47.0104 46.5816Z" stroke="#2D2940"/>
+                    <path d="M47.1307 46.7291C44.0332 60.0699 27.6598 92.3358 12.8127 76.6159C-4.18492 58.6191 38.2848 48.9627 47.1307 46.7291Z" stroke="#2D2940"/>
+                    <path d="M47.2959 46.5512C34.0762 50.0021 -2.02507 50.4627 4.95454 29.9742C12.9451 6.51815 41.2119 39.7357 47.2959 46.5512Z" stroke="#2D2940"/>
+                    <path d="M47.1655 46.6742C50.9789 33.5212 69.0709 2.19172 83.0473 18.6948C99.048 37.5882 56.119 44.9242 47.1655 46.6742Z" stroke="#2D2940"/>
+                  </svg>
+                </div>
+                <a href="#">Works</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </main>
     </div>
   )
