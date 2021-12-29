8ed80a35181da7cd58bb3d273d3a5d80d7f7dda1
Aliya Miranda <miranda.aliya@gmail.com>
2021-12-29 21:41:23 +0800

    changed misspelling in image in about

--- pages/about/index.js
+++ pages/about/index.js
@@ -25,7 +25,7 @@ export default function About(req, res) {
           </section>
           <section id="content" className={styles.content}>
             <div className={styles.container}>
-              <Image loader={imgLoader} src="img/selfie1.jpg" width={292} height={355} layout="intrinsic" alt="Profile madreloidpx"/>
+              <Image loader={imgLoader} src="img/selfie1.JPG" width={292} height={355} layout="intrinsic" alt="Profile madreloidpx"/>
             </div>
           </section>
         </div>
